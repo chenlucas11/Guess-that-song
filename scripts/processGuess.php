898d77103d645584b3897456436a684e66999963
Maddy-mojojojo <Maddie2030@sbcglobal.net>
2020-04-29 20:35:24 -0700

    changed api album/source. Also struggling to figure out how to get info from spotify

--- scripts/processGuess.php
+++ scripts/processGuess.php
@@ -1,16 +1,22 @@
 <?php
     if($_SERVER["REQUEST_METHOD"] =="POST"){
-        $songName = $_POST["songName"];
-        $artistName = $_POST["artistName"];
+        $songNameGuess = $_POST["songName"];
+        $artistNameGuess = $_POST["artistName"];
+        $songName = $_POST["songName"]; //this doesnt work--just a placeholder
+        $artistName = $_POST["artistName"]; //this doesnt work--just a placeholder
+
     }
+
+
+
 ?>
 <body> 
     <h3>Your guess:</h3>
+        <p>Song Name: <?php echo $songNameGuess; ?></p>
+        <p>Artist Name: <?php echo $artistNameGuess; ?></p>
+        <p>Time Elapsed: <?php echo "time"; ?></p>
+    <h3>Answers:</h3>
         <p>Song Name: <?php echo $songName; ?></p>
         <p>Artist Name: <?php echo $artistName; ?></p>
-        <p>Time Elapsed: <?php echo getTimeElapsed(); ?></p>
-    <h3>Answers:</h3>
-        <p>Song Name: <?php echo getSong(); ?></p>
-        <p>Artist Name: <?php echo getArtist(); ?></p>
-        <p>Points won: <?php echo 1000-getTimeElapsed();?></p>
+        <p>Points won: <?php echo "points";?></p>
 </body>
