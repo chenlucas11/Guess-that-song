f18de899521dc161eff3c08b0bb718c9d0928f0b
Maddy-mojojojo <Maddie2030@sbcglobal.net>
2020-04-30 15:34:32 -0700

    fiddling

--- processGuess.php
+++ processGuess.php
@@ -2,11 +2,13 @@
     if($_SERVER["REQUEST_METHOD"] =="POST"){
         $songNameGuess = $_POST["songName"];
         $artistNameGuess = $_POST["artistName"];
-        $songName = $_POST["item".name];
-        $artistName = $_POST["item".artists];
-        $timeStamp = $_POST["timestamp"];
-
+        //$songID = $_POST["something"];
     }
+    //the below is something I found online to put JSON info into PHP objects?
+    // Takes raw data from the request
+    $json = file_get_contents('php://v1/tracks/{$songID}');
+    // Converts it into a PHP object
+    $data = json_decode($json);
 
 
 
@@ -15,9 +17,8 @@
     <h3>Your guess:</h3>
         <p>Song Name: <?php echo $songNameGuess; ?></p>
         <p>Artist Name: <?php echo $artistNameGuess; ?></p>
-        <p>Time Elapsed: <?php echo $timeStamp; ?></p>
     <h3>Answers:</h3>
         <p>Song Name: <?php echo $songName; ?></p>
         <p>Artist Name: <?php echo $artistName; ?></p>
-        <p>Points won: <?php echo $timeStamp*10;?></p>
+        <p>Points won: <?php echo "nope";?></p>
 </body>
