ef546f506d94d7f6fe4a36050fd2f2a8077b020a
Kcunneen <kecunneen@gmail.com>
2020-04-30 19:12:12 -0400

    Update project3.js

---
+++ project3.js
@@ -0,0 +1,29 @@
+var leaderboard = function(score){
+    var i = 1;
+    while(i<11){
+        if(score > document.getElementById(i).innerHTML){
+            var temp = document.getElementById(i).innerHTML;
+            document.getElementById(i).innerHTML = score;
+            for(var x = i; x < 11; x++){
+                score = temp;
+                temp = document.getElementById(i+1).innerHTML;
+                document.getElementById(i+1).innerHTML = score;
+            }
+        i = 11;
+        }
+        else{
+            i++;
+        }
+    }
+
+    var getSong = function(){
+        var request = new XMLHttpRequest();
+        request.open("Get", "https://api.spotify.com/v1/tracks/{id}", true);
+        request.onload = function(){
+            var data = JSON.parse(this.response);
+            var name = data.name;
+            var artist = data.artists;
+        }
+    }
+
+}
