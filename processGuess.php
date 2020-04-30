47b284ec69ae6057c092c9f1f5a0aacda816db4d
Maddy-mojojojo <Maddie2030@sbcglobal.net>
2020-04-30 13:03:49 -0700

    move processGuess

--- processGuess.php
+++ processGuess.php
@@ -2,8 +2,9 @@
     if($_SERVER["REQUEST_METHOD"] =="POST"){
         $songNameGuess = $_POST["songName"];
         $artistNameGuess = $_POST["artistName"];
-        $songName = $_POST["songName"]; //this doesnt work--just a placeholder
-        $artistName = $_POST["artistName"]; //this doesnt work--just a placeholder
+        $songName = $_POST["item".name];
+        $artistName = $_POST["item".artists];
+        $timeStamp = $_POST["timestamp"];
 
     }
 
@@ -14,9 +15,9 @@
     <h3>Your guess:</h3>
         <p>Song Name: <?php echo $songNameGuess; ?></p>
         <p>Artist Name: <?php echo $artistNameGuess; ?></p>
-        <p>Time Elapsed: <?php echo "time"; ?></p>
+        <p>Time Elapsed: <?php echo $timeStamp; ?></p>
     <h3>Answers:</h3>
         <p>Song Name: <?php echo $songName; ?></p>
         <p>Artist Name: <?php echo $artistName; ?></p>
-        <p>Points won: <?php echo "points";?></p>
+        <p>Points won: <?php echo $timeStamp*10;?></p>
 </body>
