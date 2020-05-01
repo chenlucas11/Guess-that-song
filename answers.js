48b2b92311c73709e61ab7e285f00235f28cdcc3
Kcunneen <kecunneen@gmail.com>
2020-04-30 23:23:13 -0400

    Update answers.js

--- answers.js
+++ answers.js
@@ -16,3 +16,174 @@ var processSong10 = function(){
         alert("<h2>You Got Neither Correct!</h2>");
         }
 }
+
+var processSong9 = function(){
+    var guessSong = document.input-form.songName.value;
+    var guessArtist = document.input-form.artistName.value;
+    var realSong = "Don't Stop The Music";
+    var realArtist = "Rihanna"
+    if((guessSong == realSong) && (guessArtist == realArtist)){
+        alert("<h2>Both Answers You Gave Are Correct!</h2>");
+    }
+    else if(!(guessArtist == realArtist) && (guessArtist == realArtist)){
+        alert("<h2>You Got The Artist Correct, But Not The Song Name!</h2>");
+    }
+    else if(!(guessArtist == realArtist) && (guessSong == realSong)){
+        alert("<h2>You Got The Song Name Correct, But Not The Artist!</h2>");
+    }
+    else if(!(guessArtist == realArtist) && !(guessSong == realSong)){
+        alert("<h2>You Got Neither Correct!</h2>");
+        }
+}
+
+var processSong8 = function(){
+    var guessSong = document.input-form.songName.value;
+    var guessArtist = document.input-form.artistName.value;
+    var realSong = "Whatcha Say";
+    var realArtist = "Jason Derulo"
+    if((guessSong == realSong) && (guessArtist == realArtist)){
+        alert("<h2>Both Answers You Gave Are Correct!</h2>");
+    }
+    else if(!(guessArtist == realArtist) && (guessArtist == realArtist)){
+        alert("<h2>You Got The Artist Correct, But Not The Song Name!</h2>");
+    }
+    else if(!(guessArtist == realArtist) && (guessSong == realSong)){
+        alert("<h2>You Got The Song Name Correct, But Not The Artist!</h2>");
+    }
+    else if(!(guessArtist == realArtist) && !(guessSong == realSong)){
+        alert("<h2>You Got Neither Correct!</h2>");
+        }
+}
+
+var processSong7 = function(){
+    var guessSong = document.input-form.songName.value;
+    var guessArtist = document.input-form.artistName.value;
+    var realSong = "Pompeii";
+    var realArtist = "Bastille"
+    if((guessSong == realSong) && (guessArtist == realArtist)){
+        alert("<h2>Both Answers You Gave Are Correct!</h2>");
+    }
+    else if(!(guessArtist == realArtist) && (guessArtist == realArtist)){
+        alert("<h2>You Got The Artist Correct, But Not The Song Name!</h2>");
+    }
+    else if(!(guessArtist == realArtist) && (guessSong == realSong)){
+        alert("<h2>You Got The Song Name Correct, But Not The Artist!</h2>");
+    }
+    else if(!(guessArtist == realArtist) && !(guessSong == realSong)){
+        alert("<h2>You Got Neither Correct!</h2>");
+        }
+}
+
+var processSong6 = function(){
+    var guessSong = document.input-form.songName.value;
+    var guessArtist = document.input-form.artistName.value;
+    var realSong = "Single Ladies (Put a Ring on It";
+    var realArtist = "Beyoncé"
+    if((guessSong == realSong) && (guessArtist == realArtist)){
+        alert("<h2>Both Answers You Gave Are Correct!</h2>");
+    }
+    else if(!(guessArtist == realArtist) && (guessArtist == realArtist)){
+        alert("<h2>You Got The Artist Correct, But Not The Song Name!</h2>");
+    }
+    else if(!(guessArtist == realArtist) && (guessSong == realSong)){
+        alert("<h2>You Got The Song Name Correct, But Not The Artist!</h2>");
+    }
+    else if(!(guessArtist == realArtist) && !(guessSong == realSong)){
+        alert("<h2>You Got Neither Correct!</h2>");
+        }
+}
+
+var processSong5 = function(){
+    var guessSong = document.input-form.songName.value;
+    var guessArtist = document.input-form.artistName.value;
+    var realSong = "Fireflies";
+    var realArtist = "Owl City"
+    if((guessSong == realSong) && (guessArtist == realArtist)){
+        alert("<h2>Both Answers You Gave Are Correct!</h2>");
+    }
+    else if(!(guessArtist == realArtist) && (guessArtist == realArtist)){
+        alert("<h2>You Got The Artist Correct, But Not The Song Name!</h2>");
+    }
+    else if(!(guessArtist == realArtist) && (guessSong == realSong)){
+        alert("<h2>You Got The Song Name Correct, But Not The Artist!</h2>");
+    }
+    else if(!(guessArtist == realArtist) && !(guessSong == realSong)){
+        alert("<h2>You Got Neither Correct!</h2>");
+        }
+}
+
+var processSong4 = function(){
+    var guessSong = document.input-form.songName.value;
+    var guessArtist = document.input-form.artistName.value;
+    var realSong = "I Gotta Feeling";
+    var realArtist = "Black Eyed Peas"
+    if((guessSong == realSong) && (guessArtist == realArtist)){
+        alert("<h2>Both Answers You Gave Are Correct!</h2>");
+    }
+    else if(!(guessArtist == realArtist) && (guessArtist == realArtist)){
+        alert("<h2>You Got The Artist Correct, But Not The Song Name!</h2>");
+    }
+    else if(!(guessArtist == realArtist) && (guessSong == realSong)){
+        alert("<h2>You Got The Song Name Correct, But Not The Artist!</h2>");
+    }
+    else if(!(guessArtist == realArtist) && !(guessSong == realSong)){
+        alert("<h2>You Got Neither Correct!</h2>");
+        }
+}
+
+var processSong3 = function(){
+    var guessSong = document.input-form.songName.value;
+    var guessArtist = document.input-form.artistName.value;
+    var realSong = "Party In The USA";
+    var realArtist = "Miley Cyrus"
+    if((guessSong == realSong) && (guessArtist == realArtist)){
+        alert("<h2>Both Answers You Gave Are Correct!</h2>");
+    }
+    else if(!(guessArtist == realArtist) && (guessArtist == realArtist)){
+        alert("<h2>You Got The Artist Correct, But Not The Song Name!</h2>");
+    }
+    else if(!(guessArtist == realArtist) && (guessSong == realSong)){
+        alert("<h2>You Got The Song Name Correct, But Not The Artist!</h2>");
+    }
+    else if(!(guessArtist == realArtist) && !(guessSong == realSong)){
+        alert("<h2>You Got Neither Correct!</h2>");
+        }
+}
+
+var processSong10 = function(){
+    var guessSong = document.input-form.songName.value;
+    var guessArtist = document.input-form.artistName.value;
+    var realSong = "Hey, Soul Sister";
+    var realArtist = "Train"
+    if((guessSong == realSong) && (guessArtist == realArtist)){
+        alert("<h2>Both Answers You Gave Are Correct!</h2>");
+    }
+    else if(!(guessArtist == realArtist) && (guessArtist == realArtist)){
+        alert("<h2>You Got The Artist Correct, But Not The Song Name!</h2>");
+    }
+    else if(!(guessArtist == realArtist) && (guessSong == realSong)){
+        alert("<h2>You Got The Song Name Correct, But Not The Artist!</h2>");
+    }
+    else if(!(guessArtist == realArtist) && !(guessSong == realSong)){
+        alert("<h2>You Got Neither Correct!</h2>");
+        }
+}
+
+var processSong1 = function(){
+    var guessSong = document.input-form.songName.value;
+    var guessArtist = document.input-form.artistName.value;
+    var realSong = "Replay";
+    var realArtist = "Iyaz"
+    if((guessSong == realSong) && (guessArtist == realArtist)){
+        alert("<h2>Both Answers You Gave Are Correct!</h2>");
+    }
+    else if(!(guessArtist == realArtist) && (guessArtist == realArtist)){
+        alert("<h2>You Got The Artist Correct, But Not The Song Name!</h2>");
+    }
+    else if(!(guessArtist == realArtist) && (guessSong == realSong)){
+        alert("<h2>You Got The Song Name Correct, But Not The Artist!</h2>");
+    }
+    else if(!(guessArtist == realArtist) && !(guessSong == realSong)){
+        alert("<h2>You Got Neither Correct!</h2>");
+        }
+}
